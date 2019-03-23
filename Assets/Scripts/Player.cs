0234b25c7a7ebb98eef01d249e5bbc8923d63ff6
Rascal <andre.boevink@me.com>
2019-03-23 16:24:24 +0100

    194

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     // Config 
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
 
     // State
     bool isAlive = true;
@@ -32,9 +33,9 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        ClimbLadder();
         FlipSprite();
     }
-
 
     private void Run()
     {
@@ -44,10 +45,15 @@ public class Player : MonoBehaviour
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+        myAnimator.SetBool("Climbing", !playerHasHorizontalSpeed);
+
     }
 
     private void Jump()
     {
+        myAnimator.SetBool("Running", false);
+        myAnimator.SetBool("Climbing", false);
+
         int layer = LayerMask.GetMask("Ground");
         bool isTouchingLayer = myCollider2D.IsTouchingLayers(layer);
         if (CrossPlatformInputManager.GetButtonDown("Jump") && isTouchingLayer)
@@ -56,6 +62,26 @@ public class Player : MonoBehaviour
             myRigidBody.velocity += jumpVelocityToAdd;
         }
     }
+
+    private void ClimbLadder()
+    {
+        int layer = LayerMask.GetMask("Climbing");
+        bool isTouchingLadder = myCollider2D.IsTouchingLayers(layer);
+        if (!isTouchingLadder)
+        {
+            return;
+        }
+
+        float controlThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        Vector2 climbVelocity = new Vector2(myRigidBody.velocity.x, controlThrow * climbSpeed);
+        myRigidBody.velocity = climbVelocity;
+
+        bool playerHasVerticalSpeed = Mathf.Abs(myRigidBody.velocity.y) > Mathf.Epsilon;
+        myAnimator.SetBool("Climbing", playerHasVerticalSpeed);
+        myAnimator.SetBool("Running", !playerHasVerticalSpeed);
+
+    }
+
 
     private void FlipSprite()
     {
